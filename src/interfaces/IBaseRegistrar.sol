0edf15c89c7089f0669e4bad7de16c955cedbad3
realisation <realisation@protonmail.com>
2023-09-13 20:00:52 -0400

    IBaseRegistrar.sol

---
+++ src/interfaces/IBaseRegistrar.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.8.4;
+
+interface IBaseRegistrar {
+
+    event ControllerAdded(address indexed controller);
+    event ControllerRemoved(address indexed controller);
+
+    function commitments(bytes32) external view returns (uint);
+    function minCommitmentAge() external view returns (uint);
+    function maxCommitmentAge() external view returns (uint);
+    function controllers(address) external view returns (bool);
+    function baseNode() external view returns (uint);
+
+    function addController(address controller) external;
+    function removeController(address controller) external;
+
+    function setMaxCommitmentAge(uint duration) external;
+    function setMinCommitmentAge(uint duration) external;
+
+    function setResolver(address resolver) external;
+
+    function available(uint256 id) external view returns (bool);
+
+    function makeCommitment(
+        bytes memory name,
+        address owner,
+        bytes32 secret,
+        address resolver,
+        bytes[] calldata resolverData,
+        bool reverseRegistrar,
+        uint16 ownerControlledFuses
+    ) external view returns (
+        bytes32 commitment
+    );
+
+
+}
