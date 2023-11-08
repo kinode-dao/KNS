bd3d50fbc33e1d5d74c6f15ecd87b789e9ece196
rotas <rotas@rotass-MacBook-Pro.local>
2023-11-08 17:08:52 -0500

    initial qns entrypoint

---
+++ src/ENSEntrypoint.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ~0.8.17;
+
+import { ILayerZeroEndpoint } from "layer-zero/interfaces/ILayerZeroEndpoint.sol";
+
+contract QnsEnsEntrypoint {
+
+    ILayerZeroEndpoint public lz;
+    uint16 public lzc;
+    address endpoint;
+
+    constructor (address _endpoint, address _lz, uint16 _lzc) {
+
+        endpoint = _endpoint;
+        lz = ILayerZeroEndpoint(_lz);
+        lzc = _lzc;
+
+    }
+
+}
