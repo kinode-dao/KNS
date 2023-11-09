36c2c8c5bf6869af70597fc3a270d0ad8cdd9b18
operarotas <james.foley@proton.me>
2023-11-09 17:48:56 -0500

    additional configuration in entry and authorization of ownership over ens name before sending lz message

--- src/QnsEnsEntry.sol
+++ src/QnsEnsEntry.sol
@@ -2,30 +2,50 @@
 pragma solidity ~0.8.17;
 
 import { ILayerZeroEndpoint } from "layer-zero/interfaces/ILayerZeroEndpoint.sol";
+import { INameWrapper } from "ens-contracts/wrapper/INameWrapper.sol";
+import { ENS } from "ens-contracts/registry/ENS.sol";
+
+import { console } from "forge-std/console.sol";
+
+import { BytesUtils } from "./lib/BytesUtils.sol";
 import { IQnsEnsExit } from "./interfaces/IQnsEnsExit.sol";
 
 contract QnsEnsEntry {
 
+    error NotEnsOwner();
+
     address immutable public owner;
 
     ILayerZeroEndpoint 
-           immutable public lz;
-    uint16 immutable public lzc;
+           public immutable lz;
+    uint16 public immutable lzc;
 
-    bytes  public endpointpath;
-    uint16 immutable public endpointlzc;
+    bytes  public exitpath;
+    uint16 public immutable exitlzc;
+
+    address public immutable ensregistry;
+    address public immutable ensnamewrapper;
 
     modifier onlyowner () {   require(msg.sender == owner); _; }
 
+    constructor (
+        address _ensregistry,
+        address _ensnamewrapper,
+        address _lz, 
+        uint16 _lzc, 
+        address _exit, 
+        uint16 _exitlzc
+    ) {
 
-    constructor (address _lz, uint16 _lzc, address _endpoint, uint16 _endpointlzc) {
+        ensregistry = _ensregistry;
+        ensnamewrapper = _ensnamewrapper;
 
         owner = msg.sender;
         lz = ILayerZeroEndpoint(_lz);
         lzc = _lzc;
 
-        endpointpath = abi.encodePacked(_endpoint, address(this));
-        endpointlzc = _endpointlzc;
+        exitpath = abi.encodePacked(_exit, address(this));
+        exitlzc = _exitlzc;
 
     }
 
@@ -38,11 +58,32 @@ contract QnsEnsEntry {
 
     }
 
+    function setQnsRecords (
+        bytes calldata fqdn, 
+        bytes[] calldata data
+    ) public {
+
+        bytes32 node = BytesUtils.namehash(fqdn, 0);
+
+        address owner = ENS(ensregistry).owner(node);
+
+        if (owner == ensnamewrapper) owner = 
+            INameWrapper(ensnamewrapper).ownerOf(uint(node));
+        
+        if (owner != msg.sender) revert NotEnsOwner();
+
+        bytes memory payload = abi.encodeWithSelector
+            (IQnsEnsExit.setQnsRecords.selector, fqdn, data);
+
+        lzSend(payload, msg.sender, new bytes(""));
+
+    }
+
     function lzSend (bytes memory _payload, bytes memory _params) internal {
 
         lz.send{value: address(this).balance}( 
-            endpointlzc, 
-            endpointpath, 
+            exitlzc, 
+            exitpath, 
             _payload, 
             payable(address(this)), address(0), 
             _params
