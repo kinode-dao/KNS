c8d377c24d4cf264df4669e45a9c1f7645d6fb9f
operarotas <james.foley@proton.me>
2023-11-13 15:06:38 -0500

    script to register a subdomain in namewrapper

--- script/ENS.s.sol
+++ script/ENS.s.sol
@@ -13,7 +13,10 @@ import { INameWrapper } from "ens-contracts/wrapper/INameWrapper.sol";
 
 contract EnvironmentAndScript is Script {
     uint PRIVKEY = vm.envUint("PRIVATE_KEY");
-    string name = "uqtesttesttest";
+    string name = "testuq";
+    string subname = "qutset";
+    string namesubname = "qutset.testuq";
+    string ethname = "testuq.eth";
     address owner = vm.createWallet(PRIVKEY).addr;
     uint256 duration = 31536000;
     bytes32 secret = keccak256("secret");
@@ -30,6 +33,10 @@ contract EnvironmentAndScript is Script {
 
     bytes namednswire;
     bytes32 namelabel;
+    bytes subnamednswire;
+    bytes32 subnamelabel;
+    bytes namesubnamednswire;
+    bytes ethnamednswire;
 
     constructor () {
         string[] memory inputs = new string[](3);
@@ -38,6 +45,13 @@ contract EnvironmentAndScript is Script {
         inputs[2] = name;
         namednswire = vm.ffi(inputs);
         ( namelabel, ) = BytesUtils.readLabel(namednswire, 0);
+        inputs[2] = subname;
+        subnamednswire = vm.ffi(inputs);
+        ( subnamelabel, ) = BytesUtils.readLabel(namednswire, 0);
+        inputs[2] = namesubname;
+        namesubnamednswire = vm.ffi(inputs);
+        inputs[2] = ethname;
+        ethnamednswire = vm.ffi(inputs);
     }
 }
 
@@ -83,6 +97,21 @@ contract UnwrapEthName is EnvironmentAndScript {
             namelabel,
             owner,
             owner
+        );
+
+    }
+}
+
+contract RegisterSubNodeWithWrapper is EnvironmentAndScript {
+    function run () public {
+        vm.selectFork(RPC_GOERLI);
+        vm.broadcast(PRIVKEY);
+        bytes32 node = nameWrapper.setSubnodeOwner(
+            BytesUtils.namehash(ethnamednswire, 0),
+            subname,
+            owner,
+            0,
+            uint64(duration)
         );
 
     }
