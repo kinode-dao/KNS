ea58d3f860bb8cffed7040bc91ebb5d3f1e1bf4f
operarotas <james.foley@proton.me>
2023-11-27 10:14:01 -0500

    removed redundant using lib for line in DotUqRegistrar

--- src/DotUqRegistrar.sol
+++ src/DotUqRegistrar.sol
@@ -6,11 +6,9 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-import "./lib/BytesUtils.sol";
 import "./TLDRegistrar.sol";
 
 contract DotUqRegistrar is TLDRegistrar, Initializable, OwnableUpgradeable, UUPSUpgradeable {
-    using BytesUtils for bytes;
 
     function initialize (
         address _qns
