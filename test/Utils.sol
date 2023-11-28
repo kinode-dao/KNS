a70aecc8fed11310a993aae7ba34aba53b53c799
operarotas <james.foley@proton.me>
2023-11-28 14:30:23 -0500

    enhanced utils to include easy conversion to node and node id from dns string

--- test/Utils.sol
+++ test/Utils.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import {Test, console, console2} from "forge-std/Test.sol";
 
+import "../src/lib/BytesUtils.sol";
+
 contract TestUtils is Test {
 
     function dnsStringToWire (string memory name) public returns (bytes memory) {
@@ -13,6 +15,15 @@ contract TestUtils is Test {
         inputs[2] = name;
         return vm.ffi(inputs);
 
+    }
+
+    function dnsStringToNode (string memory name) public returns (bytes32) {
+        bytes memory wire = dnsStringToWire(name);
+        return BytesUtils.namehash(wire);
+    }
+
+    function dnsStringToNodeId (string memory name) public returns (uint256) {
+        return uint(dnsStringToNode(name));
     }
 
     function onERC721Received (address, address,uint256,bytes calldata) public pure returns (bytes4) {
