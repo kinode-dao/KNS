cb54160104975bfebe979805263f1415749eb5ba
operarotas <james.foley@proton.me>
2023-12-01 11:10:09 -0500

    added test to assert setting of records in registration

--- test/DotUqRegistrar.t.sol
+++ test/DotUqRegistrar.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import { console } from "forge-std/console.sol";
 import { console2 } from "forge-std/console2.sol";
 
+import { BytesUtils } from "../src/lib/BytesUtils.sol";
+
 import { DotUqShim, User, TestUtils } from "./Utils.sol";
 
 import { IDotUqRegistrar } from "../src/interfaces/IDotUqRegistrar.sol";
@@ -19,6 +21,7 @@ error CannotRevokeControlFromTLD();
 error NotAuthorized();
 
 contract DotUqTest is TestUtils {
+    using BytesUtils for bytes;
 
     bytes32 constant public ATTRIBUTES1 = 0x0000000000000000000000000000000000000000101010101010101010101010;
     bytes32 constant public ATTRIBUTES2 = 0x0000000000000000000000000000000000000000111111111111111111111111;
@@ -175,14 +178,6 @@ contract DotUqTest is TestUtils {
 
         dotuq.revokeControlOverSubdomain(_fqdn3l);
 
-        // _3LDNode = dotuq.getNode(_3LNodeId);
-
-        // assertEq(
-        //     _3LDNode & PARENT_CANNOT_CONTROL, 
-        //     PARENT_CANNOT_CONTROL,
-        //     "3LD node should have parent control revoked"
-        // );
-
     }
 
 
@@ -215,6 +210,69 @@ contract DotUqTest is TestUtils {
         qns.setKey(bytes32(_3LNodeId), keccak256("key"));
 
         assertEq(qns.key(bytes32(_3LNodeId)), keccak256("key"));
+
+    }
+
+    function testRegisterWithRecordData () public {
+
+        bytes memory _fqdn = dnsStringToWire("sub.uq");
+
+        bytes32 _nodeHash = _fqdn.namehash(0);
+
+        bytes32[] memory _routers = new bytes32[](3);
+        _routers[0] = keccak256(abi.encodePacked(uint(0)));
+        _routers[1] = keccak256(abi.encodePacked(uint(1)));
+        _routers[2] = keccak256(abi.encodePacked(uint(2)));
+
+        bytes[] memory _data = new bytes[](7);
+
+        _data[0] = abi.encodeWithSelector(
+            QNSRegistryResolver.setKey.selector,
+            _nodeHash, keccak256("key"));
+
+        _data[1] = abi.encodeWithSelector(
+            QNSRegistryResolver.setRouters.selector,
+            _nodeHash, _routers);
+
+        _data[2] = abi.encodeWithSelector(
+            QNSRegistryResolver.setIp.selector,
+            _nodeHash, type(uint128).max);
+
+        _data[3] = abi.encodeWithSelector(
+            QNSRegistryResolver.setWs.selector,
+            _nodeHash, type(uint16).max);
+
+        _data[4] = abi.encodeWithSelector(
+            QNSRegistryResolver.setWt.selector,
+            _nodeHash, type(uint16).max); 
+
+        _data[5] = abi.encodeWithSelector(
+            QNSRegistryResolver.setTcp.selector,
+            _nodeHash, type(uint16).max);
+
+        _data[6] = abi.encodeWithSelector(
+            QNSRegistryResolver.setUdp.selector,
+            _nodeHash, type(uint16).max);
+
+        bytes32 _node = bytes32(dotuq.register(_fqdn, _data));
+
+        assertEq(dotuq.ownerOf(uint(_node)), address(this), "owner should be testing contract");
+
+        bytes32[] memory _setRouters = qns.routers(_node);
+
+        assertEq(_setRouters.length, 3, "routers should be set with 3 routers");
+        assertEq(_setRouters[0], _routers[0], "unexpected first router");
+        assertEq(_setRouters[1], _routers[1], "unexpected second router");
+        assertEq(_setRouters[2], _routers[2], "unexpected third router");
+
+        assertEq(qns.key(_node), keccak256("key"), "key should be set");
+
+        ( uint128 _ip, uint16 _ws, uint16 _wt, uint16 _tcp, uint16 _udp ) = qns.ip(_node);
+        assertEq(_ip, type(uint128).max, "unexpected ip");
+        assertEq(_ws, type(uint16).max, "unexpected ws");
+        assertEq(_wt, type(uint16).max, "unexpected wt");
+        assertEq(_tcp, type(uint16).max, "unexpected tcp");
+        assertEq(_udp, type(uint16).max, "unexpected udp");
 
     }
 
