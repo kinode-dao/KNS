bfa92a0e91506c2d2ef9d31b32888cde88537659
commercium-sys <system@commercium.systems>
2024-02-17 20:57:57 -0500

    update to KNS ENS script

--- script/KNSENS.s.sol
+++ script/KNSENS.s.sol
@@ -15,16 +15,16 @@ import {KNSEnsExit} from "../src/KNSEnsExit.sol";
 import {KNSEnsEntry} from "../src/KNSEnsEntry.sol";
 
 contract EnvironmentAndScript is Script {
-    uint ENTRY_RPC = vm.createFork(vm.envString("RPC_GOERLI"));
+    uint ENTRY_RPC = vm.createFork(vm.envString("RPC_SEPOLIA"));
     uint EXIT_RPC = vm.createFork(vm.envString("RPC_SEPOLIA"));
     uint PRIVKEY = vm.envUint("PRIVATE_KEY");
-    IKNSRegistryResolver kns =
-        IKNSRegistryResolver(vm.envAddress("KNS_TEST_KNS_REGISTRY"));
-    KNSEnsExit exit = KNSEnsExit(vm.envAddress("KNS_SEPOLIA_ENS_EXIT"));
-    KNSEnsEntry entry =
-        KNSEnsEntry(payable(vm.envAddress("KNS_GOERLI_ENS_ENTRY")));
-    address ensregistry = vm.envAddress("ENS_GOERLI_REGISTRY");
-    address ensnamewrapper = vm.envAddress("ENS_GOERLI_NAME_WRAPPER");
+
+    IKNSRegistryResolver kns = IKNSRegistryResolver(vm.envAddress("KNS_REGISTRY_SEPOLIA"));
+    KNSEnsExit exit = KNSEnsExit(vm.envAddress("KNS_ENS_EXIT_SEPOLIA"));
+    KNSEnsEntry entry = KNSEnsEntry(payable(vm.envAddress("KNS_ENS_ENTRY_SEPOLIA")));
+
+    address ensregistry = vm.envAddress("ENS_REGISTRY_SEPOLIA");
+    address ensnamewrapper = vm.envAddress("ENS_NAME_WRAPPER_SEPOLIA");
 }
 
 contract DeployENSEntry is EnvironmentAndScript {
@@ -37,8 +37,10 @@ contract DeployENSExit is EnvironmentAndScript {
 
 contract DeployEnsEntryExitPair is EnvironmentAndScript {
     function run() public {
-        vm.selectFork(EXIT_RPC);
+
+        vm.selectFork(ENTRY_RPC);
         vm.startBroadcast(PRIVKEY);
+
         exit = new KNSEnsExit(
             address(kns),
             vm.envAddress("LZ_EP_SEPOLIA"),
@@ -53,51 +55,43 @@ contract DeployEnsEntryExitPair is EnvironmentAndScript {
 
         kns.registerTLD(eth, address(exit));
 
-        vm.stopBroadcast();
-        vm.selectFork(ENTRY_RPC);
-        vm.startBroadcast(PRIVKEY);
         entry = new KNSEnsEntry(
             ensregistry,
             ensnamewrapper,
-            vm.envAddress("LZ_EP_GOERLI"),
-            uint16(vm.envUint("LZ_CID_GOERLI")),
+            vm.envAddress("LZ_EP_SEPOLIA"),
+            uint16(vm.envUint("LZ_CID_SEPOLIA")),
             address(exit),
             uint16(vm.envUint("LZ_CID_SEPOLIA"))
         );
-        address(entry).call{value: .25 ether}("");
 
-        vm.stopBroadcast();
-        vm.selectFork(EXIT_RPC);
-        vm.startBroadcast(PRIVKEY);
-        exit.setEntry(address(entry), uint16(vm.envUint("LZ_CID_GOERLI")));
+        address(entry).call{value: .1 ether}("");
 
-        vm.stopBroadcast();
-        vm.selectFork(ENTRY_RPC);
-        vm.startBroadcast(PRIVKEY);
+        exit.setEntry(address(entry), uint16(vm.envUint("LZ_CID_SEPOLIA")));
+
         entry.ping();
 
-        inputs[2] = "qutset.testuq.eth";
-        bytes memory testuqbar = vm.ffi(inputs);
-        uint256 testuqbarnode = uint(BytesUtils.namehash(testuqbar, 0));
+        inputs[2] = "kinotest.eth";
+        bytes memory kinotest = vm.ffi(inputs);
+        uint256 kinotestnode = uint(BytesUtils.namehash(kinotest, 0));
 
         bytes[] memory data = new bytes[](3);
         data[0] = abi.encodeWithSelector(
             IKNSRegistryResolver.setKey.selector,
-            testuqbarnode,
+            kinotestnode,
             keccak256("yes")
         );
         data[1] = abi.encodeWithSelector(
             IKNSRegistryResolver.setIp.selector,
-            testuqbarnode,
+            kinotestnode,
             type(uint128).max
         );
         data[2] = abi.encodeWithSelector(
             IKNSRegistryResolver.setWs.selector,
-            testuqbarnode,
+            kinotestnode,
             type(uint16).max
         );
 
-        entry.setKNSRecords(testuqbar, data);
+        entry.setKNSRecords(kinotest, data);
 
         vm.stopBroadcast();
     }
@@ -110,9 +104,9 @@ contract SetWsForEnsNameOnKNS is EnvironmentAndScript {
         string[] memory inputs = new string[](3);
         inputs[0] = "./dnswire/target/debug/dnswire";
         inputs[1] = "--to-hex";
-        inputs[2] = "uqtesttest.eth";
-        bytes memory testuqbarnode = vm.ffi(inputs);
-        bytes32 namehash = BytesUtils.namehash(testuqbarnode, 0);
+        inputs[2] = "kinotest.eth";
+        bytes memory kinotestnode = vm.ffi(inputs);
+        bytes32 namehash = BytesUtils.namehash(kinotestnode, 0);
         kns.setKey(namehash, keccak256("key"));
         kns.setIp(namehash, type(uint128).max);
         kns.setWs(namehash, type(uint16).max);
@@ -135,21 +129,34 @@ contract SimulateKNSEnsExit is EnvironmentAndScript {
         address from = vm.createWallet(vm.envUint("PRIVATE_KEY")).addr;
         vm.startPrank(from);
 
-        exit = new KNSEnsExit(
+        KNSEnsExit code = new KNSEnsExit(
             address(kns),
             vm.envAddress("LZ_EP_SEPOLIA"),
             uint16(vm.envUint("LZ_CID_SEPOLIA"))
         );
 
-        string[] memory inputs = new string[](3);
-        inputs[0] = "./dnswire/target/debug/dnswire";
-        inputs[1] = "--to-hex";
-        inputs[2] = "eth";
-        bytes memory eth = vm.ffi(inputs);
-
-        kns.registerTLD(eth, address(exit));
+        vm.etch(address(exit), address(code).code);
 
         exit.simulate(vm.envBytes("PAYLOAD"));
+
+    }
+}
+
+interface IEndpoint {
+    function retryPayload(uint16 _srcChainId, bytes calldata _srcAddress, bytes calldata _payload) external;
+}
+
+contract ReplayPayload is EnvironmentAndScript {
+    function run() public {
+        vm.selectFork(EXIT_RPC);
+        vm.startBroadcast(PRIVKEY);
+
+        IEndpoint(vm.envAddress("LZ_EP_SEPOLIA"))
+            .retryPayload(
+                uint16(vm.envUint("LZ_CID_SEPOLIA")),
+                vm.envBytes("SRC_ADDR"),
+                vm.envBytes("PAYLOAD")
+            );
     }
 }
 
@@ -161,35 +168,5 @@ contract NameHash is EnvironmentAndScript {
         inputs[2] = "eth";
         bytes memory node = vm.ffi(inputs);
         console.log("node", uint(BytesUtils.namehash(node, 0)));
-    }
-}
-
-contract Thing is EnvironmentAndScript {
-    error MyError(address, address);
-
-    function run() public {
-        (bool success, bytes memory data) = address(this).call(
-            abi.encodeWithSelector(this.thing.selector)
-        );
-        if (!success) {
-            console.log("!success");
-            console.logBytes(data);
-            bytes4 selector;
-            assembly {
-                selector := mload(add(data, 0x20))
-            }
-            console.logBytes4(selector);
-            // (bytes4 sel, address one, address two) = abi.decode(data, (bytes4, address,address));
-            bytes4 sel = abi.decode(data, (bytes4));
-            // console.logBytes4(abi.decode(data, (bytes4,address,address)));
-            // console.log("one", one);
-            // console.log("two", two);
-            // console.log("sel");
-            // console.logBytes8(selector);
-        }
-    }
-
-    function thing() external {
-        revert MyError(address(this), address(this));
     }
 }
